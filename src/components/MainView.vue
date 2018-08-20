0f4b487989094b50d9e438a1e1aaa5ce187860c6
httishere <18457730959@163.com>
2018-08-20 15:31:18 +0800

    main-container

---
+++ src/components/MainView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-container">
+    main
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
